8986ecb3f0edd8ec12aaedd7b6573b1eb1dd77aa
kang wang <>
2022-03-07 16:33:46 +0800

    代码修改

---
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/config/MybatisPlusConfig.java
@@ -0,0 +1,7 @@
+package org.linlinjava.litemall.db.config;/**
+   *
+   *@Description TODO
+   *@Author kangwang
+   *@Date 6:52 下午 2022/2/26
+*/public class MybatisPlusConfig {
+}
---
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/service/CommonService.java
@@ -0,0 +1,7 @@
+package org.linlinjava.litemall.db.service;/**
+   *
+   *@Description TODO
+   *@Author kangwang
+   *@Date 9:14 上午 2022/2/26
+*/public class CommonService {
+}
